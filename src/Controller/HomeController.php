f580bd594a0386992ad1d23378689a46cde798d1
Olha <olha.idf@gmail.com>
2020-11-23 17:30:41 +0100

    swiftmailer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Repository\CarteRepository;
 use App\Repository\ArticlesRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,9 +15,39 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(CarteRepository $carteRepository, ArticlesRepository $articlesRepository)
+    public function index(CarteRepository $carteRepository, ArticlesRepository $articlesRepository,\Swift_Mailer $mailer, Request $request )
     {
+        $formulaireContact = $this->createForm(ContactType::class);
+        $formulaireContact->handleRequest($request);
+
+        if($formulaireContact->isSubmitted() && $formulaireContact->isValid()){
+            $infos = $formulaireContact->getData();
+            // On crée le message
+            $mail = (new \Swift_Message('Nouveau contact'))
+                ->setFrom($infos['email'])
+                ->setTo('restaurant.lacarotte@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'contact/email.html.twig', [
+                            'nom' => $infos['nom'],
+                            'prenom' => $infos['prenom'],
+                             'email' => $infos['email'],
+                            'message' => $infos['message']
+                        ],
+                        'text/html'
+                    )
+                );
+            $mailer->send($mail);
+            $this->addFlash(
+                'success',
+                'Votre message a bien été envoyé'
+            );
+            
+        }
+
+
        
+    
        $carte = $carteRepository->findAll();
        $articles = $articlesRepository->findAll();
       
@@ -23,6 +55,7 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'carte' => $carte,
             'articles' => $articles,
+            'formulaireDeContact' => $formulaireContact->createView()
             
         ]);
     }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+                        ->add('email', EmailType::class)
+           
+            ->add('message', TextareaType::class)
+            
+            ->add('RGPD', CheckboxType::class, [
+               
+                
+                        'label' => " En cochant cette case et en soumettant ce
+                        formulaire, j'accepte que mes données personnelles soient utilisées pour me recontacter dans le cadre de ma
+                        demande indiquée dans ce formulaire. Aucun autre traitement ne sera effectué avec mes informations.",
+                    ])
+                
+            
+           ->add('envoyer', SubmitType::class, [
+            'attr' => [
+                'class' => 'btn btn-sm text-center mt-2 float-right'],
+        ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
